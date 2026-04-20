68877af9f93aea41b4f0b3a3c689dad9e4982d19
sdzabu2011 <abdullohmiramatov@gmail.com>
2026-04-20 15:48:53 +0500

    fx

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,10 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/login", // Agar maxsus login sahifangiz bo'lsa
+  },
+})
 
 export const config = {
   matcher: ['/chat/:path*', '/settings/:path*', '/artifacts/:path*']
